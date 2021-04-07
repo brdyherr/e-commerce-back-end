b0ac15d1feedef68c3d6aa95da3a609bf0d32b0e
Brown Mamba <brdyherrera@gmail.com>
2021-04-07 06:48:37 -0400

    added first association

--- models/index.js
+++ models/index.js
@@ -5,7 +5,9 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'user_id'
+});
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)

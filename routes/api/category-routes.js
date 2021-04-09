66379f606c809d05d29a2eb226380c29ce6c4f68
Brown Mamba <brdyherrera@gmail.com>
2021-04-09 01:54:28 -0400

    completed category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -28,27 +28,27 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+router.get('/:id', (req, res) => {
   Category.findOne({
-    attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
     },
+    attributes: [
+      'id',
+      'category_name'
+    ],
     include: [
-      {
-        model: Product,
-        attributes: ['id', 'title', 'post_url', 'created_at']
-      },
+      Product
     ]
   })
-    .then(dbUserData => {
-      if (!dbUserData) {
+    .then(categoryData => {
+      if (!categoryData) {
         res.status(404).json({ message: 'No user found with this id' });
         return;
       }
-      res.json(dbUserData);
+      res.json(categoryData);
     })
     .catch(err => {
       console.log(err);
@@ -56,16 +56,46 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
   // create a new category
+  router.post('/', (req, res) => {
+    Category.create({
+      category_name: req.body.category_name,
+    })
+      .then((categoryData) => 
+      res.json(categoryData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
 });
 
-router.put('/:id', (req, res) => {
+
   // update a category by its `id` value
+router.put('/:id', (req, res) => { 
+  Category.update({
+    category_name: req.body.category_name
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categoryData => {
+    res.json(categoryData)
+  });
 });
 
+
+// delete a category by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).
+  then(categoryData => {
+    res.json(categoryData)
+  });
 });
 
 module.exports = router;
